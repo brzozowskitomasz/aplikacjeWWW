ec726a13418035d865579af10cad53c5be7f51f5
Tomasz Brzozowski <tomsaz@wp.pl>
2019-10-10 19:19:39 +0200

    Zadanie13

---
+++ Zadanie13.py
@@ -0,0 +1,5 @@
+Tomasz_Brzozowski={"numer_indeksu":"141050"}
+Ktos={"numer_indeksu":"10000","wiek":22}
+lista=[Tomasz_Brzozowski,Ktos]
+print("numer_indeksu: ",Tomasz_Brzozowski["numer_indeksu"],"Wiek:",Ktos["wiek"])
+print(lista)
