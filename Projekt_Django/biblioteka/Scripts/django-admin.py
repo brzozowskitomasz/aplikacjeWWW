c42da56ac154efd3552c0f3636279a6cad7234a0
Tomasz Brzozowski <tomsaz@wp.pl>
2019-11-14 13:10:30 +0100

    Projekt Django

---
+++ Projekt_Django/biblioteka/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!d:\repozytorium\aplikacjewww\projekt_django\biblioteka\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ Projekt_Django/biblioteka/src/biblioteka/biblioteka1/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Biblioteka1Config(AppConfig):
+    name = 'biblioteka1'
