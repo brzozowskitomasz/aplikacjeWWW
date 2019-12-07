afd550c55956bbafe4fd73cd4ec6a999b9fbd825
Tomasz Brzozowski <tomsaz@wp.pl>
2019-12-07 23:02:28 +0100

    widoki

--- Projekt_Django/biblioteka/src/biblioteka/biblioteka1/admin.py
+++ Projekt_Django/biblioteka/src/biblioteka/biblioteka1/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import wypozyczenia
+from .models import kategorie
+from .models import wydawnictwo
+from .models import autorzy
+from .models import ksiazki
+from .models import czytelnicy
+
+admin.site.register(wypozyczenia)
+admin.site.register(kategorie)
+admin.site.register(wydawnictwo)
+admin.site.register(autorzy)
+admin.site.register(ksiazki)
+admin.site.register(czytelnicy)
 # Register your models here.
--- Projekt_Django/biblioteka/src/biblioteka/biblioteka1/models.py
+++ Projekt_Django/biblioteka/src/biblioteka/biblioteka1/models.py
@@ -1,23 +1,29 @@
 from django.db import models
 
+
 class wypozyczenia(models.Model):
     data_wypozycz = models.DateTimeField('data wypozyczenia')
     data_oddania = models.DateTimeField('data oddania')
 
+
 class kategorie(models.Model):
     nazwa = models.CharField(max_length=45)
 
+
 class wydawnictwo(models.Model):
     nazwa = models.CharField(max_length=45)
+
 
 class autorzy(models.Model):
     imie = models.CharField(max_length=45)
     nazwisko = models.CharField(max_length=45)
 
+
 class ksiazki(models.Model):
     tytul = models.CharField(max_length=45)
     opis = models.CharField(max_length=45)
     rok_wydania = models.DateTimeField('rok wydania')
+
 
 class czytelnicy(models.Model):
     login = models.CharField(max_length=45)
